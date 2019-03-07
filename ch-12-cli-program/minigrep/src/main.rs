118dcbc7bff90b0ccb8ed50f042dbe5545c0c1aa
Mihai Rotaru <mihai.rotaru@gmx.com>
2019-03-07 06:46:26 +0000

    improve error handling
    Use `Result` value and `unwrap_or_else`
    
    > The difference between unwrap and unwrap_or is that unwrap will fail if
    > there is no value (None) but unwrap_or will return the specified default
    > ("nothing" in this case)
    
    ([source](https://stackoverflow.com/a/21258003))

--- ch-12-cli-program/minigrep/src/main.rs
+++ ch-12-cli-program/minigrep/src/main.rs
@@ -1,12 +1,16 @@
 use std::env;
 use std::fs::File;
 use std::io::prelude::*;
+use std::process;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let config = Config::new(&args);
-    println!("Searching for \"{}\" in \"{}\"", config.query, config.filename);
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
 
+    println!("Searching for \"{}\" in \"{}\"", config.query, config.filename);
     let mut f = File::open(config.filename).expect("File not found");
     let mut contents = String::new();
     f.read_to_string(&mut contents).expect("Could not read the file");
@@ -19,9 +23,12 @@ struct Config {
 }
 
 impl Config {
-    fn new(args: &[String]) -> Config {
+    fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("Not enough arguments");
+        }
         let query = args[1].clone();
         let filename = args[2].clone();
-        Config { query, filename }
+        Ok(Config { query, filename })
     }
 }
