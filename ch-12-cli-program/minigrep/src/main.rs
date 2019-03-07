5caf8da55d9f5a8716c4d61129fd6d1a1f5625d7
Mihai Rotaru <mihai.rotaru@gmx.com>
2019-03-07 06:21:16 +0000

    use struct for Config

--- ch-12-cli-program/minigrep/src/main.rs
+++ ch-12-cli-program/minigrep/src/main.rs
@@ -4,12 +4,22 @@ use std::io::prelude::*;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let query = &args[1];
-    let filename = &args[2];
-    println!("Searching for \"{}\" in \"{}\"", query, filename);
+    let config = parse_config(&args);
+    println!("Searching for \"{}\" in \"{}\"", config.query, config.filename);
 
-    let mut f = File::open(filename).expect("File not found");
+    let mut f = File::open(config.filename).expect("File not found");
     let mut contents = String::new();
     f.read_to_string(&mut contents).expect("Could not read the file");
     println!("With text: \n{}", contents);
+}
+
+fn parse_config(args: &[String]) -> Config {
+    let query = args[1].clone();
+    let filename = args[2].clone();
+    Config { query, filename }
+}
+
+struct Config {
+    query: String,
+    filename: String,
 }
