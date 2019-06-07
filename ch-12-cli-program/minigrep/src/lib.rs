6e72e0e855cd3e51bee649a113bf07fbb1a01dca
Mihai Rotaru <mihai.rotaru@gmx.com>
2019-06-07 06:40:28 +0100

    use closures and iterators to find matches

--- ch-12-cli-program/minigrep/src/lib.rs
+++ ch-12-cli-program/minigrep/src/lib.rs
@@ -43,23 +43,15 @@ impl Config {
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut results = Vec::new();
-    for line in contents.lines() {
-        if line.contains(query) {
-            results.push(line);
-        }
-    }
-    results
+    contents.lines()
+        .filter(|line| line.contains(query))
+        .collect()
 }
 
 pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut results = Vec::new();
-    for line in contents.lines() {
-        if line.to_lowercase().contains(&query.to_lowercase()) {
-            results.push(line);
-        }
-    }
-    results
+    contents.lines()
+        .filter(|line| line.contains(&query.to_lowercase()))
+        .collect()
 }
 
 #[cfg(test)]
