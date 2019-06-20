7148b50eb04891892801c9076ce5bc540ae75160
Mihai Rotaru <mihai.rotaru@gmx.com>
2019-06-20 06:45:47 +0100

    use RefCell, mutate the immutable!

--- ch-15-smart-pointers/refcell-limit-tracker/src/main.rs
+++ ch-15-smart-pointers/refcell-limit-tracker/src/main.rs
@@ -33,20 +33,21 @@ impl<'a, T> LimitTracker<'a, T> where T: Messenger {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use std::cell::RefCell;
 
     struct MockMessenger {
-        sent_messages: Vec<String>,
+        sent_messages: RefCell<Vec<String>>,
     }
 
     impl MockMessenger {
         fn new() -> MockMessenger {
-            MockMessenger { sent_messages: vec![] }
+            MockMessenger { sent_messages: RefCell::new(vec![]) }
         }
     }
 
     impl Messenger for MockMessenger {
         fn send(&self, message: &str) {
-            self.sent_messages.push(String::from(message));
+            self.sent_messages.borrow_mut().push(String::from(message));
         }
     }
 
@@ -55,7 +56,7 @@ mod tests {
         let mock_messenger = MockMessenger::new();
         let mut limit_tracker = LimitTracker::new(&mock_messenger, 100);
         limit_tracker.set_value(80);
-        assert_eq!(mock_messenger.sent_messages.len(), 1);
+        assert_eq!(mock_messenger.sent_messages.borrow().len(), 1);
     }
 }
 
