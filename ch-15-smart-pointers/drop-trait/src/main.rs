1184775593574c935e5b0076e4e50b2cd39c3ba7
Mihai Rotaru <mihai.rotaru@gmx.com>
2019-06-26 07:02:35 +0100

    drop clarifications

--- ch-15-smart-pointers/drop-trait/src/main.rs
+++ ch-15-smart-pointers/drop-trait/src/main.rs
@@ -10,10 +10,29 @@ impl Drop for Foo {
 
 fn main() {
     println!("before");
-    let foo_1 = Foo { data: String::from("foo 1")};
-    let foo_2 = Foo { data: String::from("foo 2")};
+    let _foo_1 = Foo { data: String::from("foo 1")};
+    let _foo_2 = Foo { data: String::from("foo 2")};
+    let _foo_3 = Foo { data: String::from("foo 3")};
+
+    // Code below won't compile. Values are dropped automatically when they go
+    // out of scope. So, if we could explicitly drop it, the drop function
+    // (destructor) will be called twice - when we call it, and when it goes out
+    // of scope. A "double-free", in other words.
+    // _foo_3.drop();
+
+    // Can use std::mem::drop to prematurely drop a value. This is in the
+    // prelude, so we can just call it without namespacing. This method is
+    // actually very simple: https://doc.rust-lang.org/std/mem/fn.drop.html
+    // Because we moved the value into the function, as soon as `drop` is done,
+    // the destructor (implemented via the `Drop` trait) will be called simply
+    // because the value goes out of scope, and there are no other references to
+    // it - because it was _moved_ into the function. Beautiful !
+    drop(_foo_3);
+
     println!("after");
-    // note that the drop function will be called after
-    // the "after" string is printed, and in reverse order.
-    // So, foo_2 will be dropped before foo_1
+
+    // Note that the drop function will be called after the "after" string is
+    // printed, and in reverse order. So, foo_2 will be dropped before foo_1.
+    // But foo_3 is dropped explicitly, so it's destructor will be called before
+    // the other ones.
 }
