042aa7d11b80d3861d3957ca0234a71d76f010c1
Mihai Rotaru <mihai.rotaru@gmx.com>
2019-10-17 10:34:47 +0100

    struct fields are private to outer module code

--- ch-17-object-oriented-features/averaged-collection/src/main.rs
+++ ch-17-object-oriented-features/averaged-collection/src/main.rs
@@ -6,49 +6,63 @@
 // mimics a C++/Java class, as instances of this struct will have data and
 // procedures associated with them.
 
-// NOTRE: All fields should be private, according to the book - unless I
-// misunderstood something. But it doesn't seem to be the case in current rust.
-// Also verified in the official playground, and fields don't seem to be
-// private:
-// https://play.rust-lang.org/?version=stable&mode=debug&edition=2018&gist=e1709ccf8727816f0ec9309599f8c8eb
-// online book: https://doc.rust-lang.org/stable/book/ch17-01-what-is-oo.html
-pub struct AveragedCollection {
-    list: Vec<i32>,
-    average: f64,
-}
-
-impl AveragedCollection {
-    pub fn add(&mut self, value: i32) {
-        self.list.push(value);
-        self.update_average();
+mod my_collections {
+    // NOTE: All fields of structs from other modules are private. Given that
+    // AveragedCollection is in a module, code outside this module will not have
+    // access to the fields - but code inside this module, does.
+    pub struct AveragedCollection {
+        list: Vec<i32>,
+        average: f64,
     }
 
-    pub fn remove(&mut self) -> Option<i32> {
-        let result = self.list.pop();
-        match result {
-            Some(value) => {
-                self.update_average();
-                Some(value)
+    impl AveragedCollection {
+        pub fn new() -> Self {
+            AveragedCollection {
+                list: Vec::new(),
+                average: 0.0,
             }
-            None => None,
+        }
+
+        pub fn add(&mut self, value: i32) {
+            self.list.push(value);
+            self.update_average();
+        }
+
+        pub fn remove(&mut self) -> Option<i32> {
+            let result = self.list.pop();
+            match result {
+                Some(value) => {
+                    self.update_average();
+                    Some(value)
+                }
+                None => None,
+            }
+        }
+
+        pub fn average(&self) -> f64 {
+            self.average
+        }
+
+        fn update_average(&mut self) {
+            let total: i32 = self.list.iter().sum();
+            self.average = total as f64 / self.list.len() as f64;
         }
     }
 
-    pub fn average(&self) -> f64 {
-        self.average
-    }
-
-    fn update_average(&mut self) {
-        let total: i32 = self.list.iter().sum();
-        self.average = total as f64 / self.list.len() as f64;
+    pub fn i_can_access_members () {
+        let mut c = AveragedCollection { list: Vec::new(), average: 0.0 };
+        c.add(10);
+        c.add(5);
+        println!("Average, field: {}", c.average); // this will work - can access fields from inside the module
     }
 }
 
 
 fn main() {
-    let mut c = AveragedCollection { list: Vec::new(), average: 0.0 };
+    let mut c = my_collections::AveragedCollection::new();
     c.add(10);
     c.add(5);
     println!("Average: {}", c.average());
-    println!("Average field: {}", c.average); // this should not work, but it does - fields are not private
+    // println!("Average field: {}", c.average); // this will not work because fields are private to code outside the module
+    my_collections::i_can_access_members(); // call a function that is inside the module - it does have access to members
 }
